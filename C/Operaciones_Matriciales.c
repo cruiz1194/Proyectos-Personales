8a9d716dc81d29788b0af95488cb3981c7788bbf
cruiz1194 <vakamacarlos19_94@outlook.com>
2017-09-05 18:24:34 -0500

    Add files via upload

---
+++ C/Operaciones_Matriciales.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+Título: Operaciones_Matriciales.c
+
+Descripción: Este programa utilizará un conjunto de operaciones matriciales para poder darle un resultado al usuario
+según lo que desee calcular. 
+
+Autor: Carlos Javier Ruiz Sansores
+*/
+int main(int argc, char *argv[]) {
+	
+	return 0;
+}
+
