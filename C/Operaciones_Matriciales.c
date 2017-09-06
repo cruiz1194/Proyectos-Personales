779412935d93ac25ceeeb2fa6f4234da8f0b7f7e
cruiz1194 <vakamacarlos19_94@outlook.com>
2017-09-06 18:45:40 -0500

    Algunos cambios como el hecho de que ya se incluye la librería de ops

--- C/Operaciones_Matriciales.c
+++ C/Operaciones_Matriciales.c
@@ -1,9 +1,10 @@
+<<<<<<< current
 #include <stdio.h>
 /**
-Título: Operaciones_Matriciales.c
+Tï¿½tulo: Operaciones_Matriciales.c
 
-Descripción: Este programa utilizará un conjunto de operaciones matriciales para poder darle un resultado al usuario
-según lo que desee calcular. 
+Descripciï¿½n: Este programa utilizarï¿½ un conjunto de operaciones matriciales para poder darle un resultado al usuario
+segï¿½n lo que desee calcular. 
 
 Autor: Carlos Javier Ruiz Sansores
 */
@@ -12,3 +13,19 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
+=======
+#include <stdio.h>
+#include "Operaciones_Matriciales.h"
+/**
+Tï¿½tulo: Operaciones_Matriciales.c
+
+Descripciï¿½n: Este programa utilizarï¿½ un conjunto de operaciones matriciales para poder darle un resultado al usuario
+segï¿½n lo que desee calcular.
+
+Autor: Carlos Javier Ruiz Sansores
+*/
+int main(int argc, char *argv[]) {
+	printf("Programa para algunas operaciones matriciales hechas en C\n");
+	return 0;
+}
+>>>>>>> before discard
