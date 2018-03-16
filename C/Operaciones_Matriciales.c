0a07b4142a5ae2c2edbb27845dcf8fb1fefb09d2
Carlos ruiz <carlos@MacBook-de-Carlos-Ruiz.local>
2018-03-15 21:17:27 -0600

    Added some dialogue to it

--- C/Operaciones_Matriciales.c
+++ C/Operaciones_Matriciales.c
@@ -5,22 +5,22 @@
 Tï¿½tulo: Operaciones_Matriciales.c
 
 Descripciï¿½n: Este programa utilizarï¿½ un conjunto de operaciones matriciales para poder darle un resultado al usuario
-segï¿½n lo que desee calcular. 
+segï¿½n lo que desee calcular.
 
 Autor: Carlos Javier Ruiz Sansores
 =======
 #include <stdio.h>
 /**
-Título: Operaciones_Matriciales.c
+Tï¿½tulo: Operaciones_Matriciales.c
 
-Descripción: Este programa utilizará un conjunto de operaciones matriciales para poder darle un resultado al usuario
-según lo que desee calcular. 
+Descripciï¿½n: Este programa utilizarï¿½ un conjunto de operaciones matriciales para poder darle un resultado al usuario
+segï¿½n lo que desee calcular.
 
 Autor: Carlos Javier Ruiz Sansores
 >>>>>>> origin/master
 */
 int main(int argc, char *argv[]) {
-	
+
 	return 0;
 }
 
@@ -38,6 +38,8 @@ Autor: Carlos Javier Ruiz Sansores
 */
 int main(int argc, char *argv[]) {
 	printf("Programa para algunas operaciones matriciales hechas en C\n");
+	printf("Esta es solo una pequena muestra de las operaciones\n", );
+	
 	return 0;
 }
 >>>>>>> before discard
